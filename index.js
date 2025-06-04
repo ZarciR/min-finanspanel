f888063f41a13afe124ef29bdbd24c601403c138
ZarciR <ola.morin+github@gmail.com>
2025-06-04 17:41:25 +0200

    Update index.js med rätt callback uri

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://min-finanspanel.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
